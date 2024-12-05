64f91d67e8a3c3992c8605b17b1be434f1759b60
ilveron <alessandromonetti@outlook.it>
2024-12-05 15:43:19 +0100

    Add complete day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,4 @@
 import os
-from math import ceil
 
 def parse_input(successors, updates):
     with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as f:
@@ -23,16 +22,37 @@ def is_update_correct(successors, update):
     return True  
 
 
-def get_part1(successors, updates):
-    result = 0
-
+def separate_correct_and_incorrect_updates(successors, updates):
+    correct, incorrect = [], []
     for update in updates:
         update_len = len(update)
         if is_update_correct(successors, update):
-            middle_page = int(update[update_len//2])
-            print(middle_page)
-            result += middle_page
+            correct.append(update)
+        else:
+            incorrect.append(update)
+
+    return correct, incorrect
+
+
+def calculate_middle_pages(successors, updates):
+    result = 0
+    for update in updates:
+        middle_page = int(update[len(update)//2])
+        result += middle_page
     return result
+
+
+def reorder_incorrect_updates(successors, updates):
+    for update in updates:
+        is_ordered = False
+        
+        while not is_update_correct(successors, update):
+            for prec in range(len(update)-1):
+                for succ in range(prec+1, len(update)):
+                    if update[succ] not in successors[update[prec]]:
+                        temp = update[prec]
+                        update[prec] = update[succ]
+                        update[succ] = temp
 
 
 def main():
@@ -40,8 +60,12 @@ def main():
     updates = []
 
     parse_input(successors, updates)
+    correct_updates, incorrect_updates = separate_correct_and_incorrect_updates(successors, updates)
 
-    print(f"part1: {get_part1(successors, updates)}")
+    reorder_incorrect_updates(successors, incorrect_updates)
+
+    print(f"part1: {calculate_middle_pages(successors, correct_updates)}")
+    print(f"part2: {calculate_middle_pages(successors, incorrect_updates)}")
     
 
 if __name__ == '__main__':
